238e03cfabf449d2e5036b323058df15b988e77b
ae <erradi@live.com>
2025-03-01 23:48:32 +0300

    Client-side JS slides and examples

--- examples/04.JavaScript/7.sets-maps/sets.js
+++ examples/04.JavaScript/7.sets-maps/sets.js
@@ -1,9 +1,12 @@
-const names = new Set();
+let names = new Set();
 names.add('Samir');
 names.add('Fatima');
 names.add('Mariam');
 names.add('Ahmed');
 names.add('Samir'); // won't be added
+
+// Or - // Samir won't be added
+names = new Set(['Samir', 'Fatima', 'Mariam', 'Ahmed', 'Samir']); 
 
 for (const name of names) {
     console.log(name);
