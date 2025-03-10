acf53163cd5ab7d788dfa9d996b0c4fcb44e84b9
emankhabaz <116437085+emankhabaz@users.noreply.github.com>
2025-03-10 09:45:31 +0300

    test

---
+++ examples/04.JavaScript/1.variables/test.js
@@ -0,0 +1,18 @@
+// const x = 4;
+// // console.log(x);
+// // console.log("Hello, world!");
+// const st = {id: 1234, firstname:'fatma', lastname:'saleh'};
+// console.log(st.id);
+
+
+
+const num = 34;
+const str= num.toString();
+console.log(str);
+
+// const fisrtname="5";
+// // const num = parseInt(fisrtname);
+// // console.log(num);
+
+// const num2= parseInt(fisrtname);
+// console.log(num2);
